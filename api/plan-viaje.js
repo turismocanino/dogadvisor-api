079c5a3dded238401d022e67004219808caaec14
turismocanino <guau@turismocanino.es>
2026-01-03 17:08:53 +0100

    Update plan-viaje.js

--- api/plan-viaje.js
+++ api/plan-viaje.js
@@ -1,6 +1,6 @@
 const { randomBytes } = require("crypto");
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   // (Opcional) CORS para poder probar desde navegador/Hoppscotch sin Proxy
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
